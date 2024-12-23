b959205e0c7cf4857b1a041e4840ba7a0dadcb38
dina3456 <dianaraiychuk@gmail.com>
2024-12-23 11:16:10 +0200

    бот.py

--- бот.py
+++ бот.py
@@ -1,12 +1,12 @@
-import telebot #Імпортується бібліотека telebot
+import telebot 
 
-token= '7727834349: AAG5JC-09jpKNr6VUENzHB5unDvgfwQmHiE' #унікальний ключ доступу Telegram-бота. Токен видається через BotFather у Telegram під час створення нового бота.
+token= '7727834349: AAG5JC-09jpKNr6VUENzHB5unDvgfwQmHiE'
 
-bot = telebot.TeleBot(token) #створення екземпляра бота.
+bot = telebot.TeleBot(token) 
 
-@bot.message_handler(content_types=["text"]) #Декоратор, який визначає обробник для певних типів повідомлень.
-def repeat_all_messages(message): #Це функція, яка буде викликатися, коли бот отримує текстове повідомлення.
-    bot.send_message(message.chat.id, message.text) #Надсилається відповідь користувачу.повторює повідомлення
+@bot.message_handler(content_types=["text"]) 
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text) 
 
-if __name__ ==  '__main__': #перевіряє, чи файл запускається напряму, а не імпортується як модуль в інший файл.Якщо файл запускається, виконується код нижче
-     bot.polling() #запускає безперервний процес опитування серверів Telegram
+if __name__ ==  '__main__':
+     bot.polling() 
